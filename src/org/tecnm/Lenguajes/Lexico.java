090a444da6787aea7ab2909856cd827eb367dd37
catneep <40044744+catneep@users.noreply.github.com>
2020-03-29 17:37:31 -0600

    Agregados comentarrios de bloque y corrección de errores

--- src/org/tecnm/Lenguajes/Lexico.java
+++ src/org/tecnm/Lenguajes/Lexico.java
@@ -37,24 +37,24 @@ public class Lexico {
         salida = new ArrayList<Lexema>();
     }
     
+    /**
+     * Llena el {@code ArrayList salida} con Lexemas encontrados dentro del código fuente separado.
+     */
     private void generar(){
         Scanner sc = new Scanner(entrada);
         String linea = ""; //variable para comparar con elementos de lista de Tokens
         boolean evaluarAutomata = false;
+        boolean noToken = false;
         int lastToken = -1;
-        //if (tokens.get(j).hasAutomata() && tokens.get(j).getAutomata().probarCadena(linea))
         int i = 0; //Línea actual en la entrada
+        
         while(sc.hasNext()){
             linea = sc.nextLine();
-            if (linea.length() >= 5 && linea.substring(0, 5).equals("<line")) { //Condición encargada de omitir evaluación números de línea
-                //System.out.println("Línea Pre Sub: " + linea);
-                i++; //asigna línea actual en código fuente
-                linea = linea.substring(6);
-                //System.out.println("Línea Post Sub:" + linea);
-            } else if (linea.length() == 6 && linea.substring(0, 5).equals("<line>")) {
-                i++; //asigna línea actual en código fuente
-            }
             
+            if (linea.equals("<line>")) { //Condición encargada de omitir evaluación números de línea
+                i++; //asigna línea actual en código fuente
+                continue;
+            }
             if (evaluarAutomata && lastToken != -1){
                 if (!tokens.get(lastToken).getAutomata().probarCadena(linea)){ //Evalúa errores de sintaxis
                     salida.add(new Lexema(linea, "E" + tokens.get(lastToken).getId(), i)); //Asigna el error en salida
@@ -62,7 +62,7 @@ public class Lexico {
                 evaluarAutomata = false;
                 lastToken = -1; //Se reinician los estados de las variables auxiliares para la próxima evaluación
             }
-            
+
             for (int j = 0; j < tokens.size(); j++){
                 if (tokens.get(j).getToken().equals(linea)) {
                     salida.add(new Lexema(linea, tokens.get(j).getId(), i));
@@ -73,35 +73,64 @@ public class Lexico {
                         }
                         else evaluarAutomata = false;
                     } catch (NullPointerException e){}
-                }
+                    break;
+                } else noToken = true;
+            }
+            if (noToken) {
+                salida.add(new Lexema(linea, 0, i));
+                noToken = false;
             }
         }
         sc.close();
    
     }
     
-    public ArrayList<Lexema> getSalida() throws IOException{
+    /**
+     * Retorna el {@code ArrayList<Lexema>} de salida, creado en {@code generar()}.
+     * @return
+     */
+    public ArrayList<Lexema> getSalida(){
         return salida;
     }
     
-    public void generarArchivo() throws IOException{
+    /**
+     * Generar un archivo con el nombre especificado y agrega la extensión {@code .txt}.
+     * @param name
+     * @throws IOException 
+     */
+    public void generarArchivo(String name) throws IOException{
         generar();
         String s = "";
         for (int i = 0; i < salida.size(); i++){
-            s += salida.get(i).getLexema() + "   " + salida.get(i).getId() + "   "  + salida.get(i).getLinea() + "\n";
+            s += salida.get(i).getLexema() + " | " + salida.get(i).getId() + " | "  + salida.get(i).getLinea() + "\n";
         }
-        File f = new File("lexico.txt");
+        File f = new File(name + ".txt");
         
-        if (f.createNewFile())
-        {
-            System.out.println(f.getName() + " creado");
-        } else {
-            System.out.println(f.getName() + " ya existe");
-        }
+        if (f.createNewFile()) System.out.println(f.getName() + " creado");
+        else System.out.println(f.getName() + " ya existe");
         
         FileWriter writer = new FileWriter(f);
         writer.write(s);
         writer.close();
+    }
+    
+    /**
+     * Generación de archivo por defecto con el nombre {@code lexico.txt} en la ruta del proyecto.
+     * @throws IOException 
+     */
+    public void generarArchivo() throws IOException{
+        generar();
+        String s = "";
+        for (int i = 0; i < salida.size(); i++){
+            s += salida.get(i).getLexema() + " | " + salida.get(i).getId() + " | "  + salida.get(i).getLinea() + "\n";
+        }
+        File f = new File("lexico.txt");
         
+        if (f.createNewFile()) System.out.println(f.getName() + " creado");
+        else System.out.println(f.getName() + " ya existe");
+        
+        FileWriter writer = new FileWriter(f);
+        writer.write(s);
+        writer.close();
     }
 }
